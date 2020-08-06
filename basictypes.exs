ff69b06de39ceae007df741eea7e046ed1c597cc
TonPaes <ton.paes.bccs@gmail.com>
2020-08-06 14:16:46 -0300

    messing around with types

--- basictypes.exs
+++ basictypes.exs
@@ -1,7 +1,6 @@
 content = "Now is the time"
 lp =
-    with {:ok, file} = File.open("/etc/passwd"),
-        content = IO.read(file, :all),
+    with {:ok, file} = File.open("/etc/passwd"), content = IO.read(file, :all),
         :ok = File.close(file),
         [_, uid, gid] = Regex.run(~r/^_lp:.*?:(\d+):(\d+)/m, content) do
             "Group: #{gid}, User: #{uid}"
@@ -10,3 +9,4 @@ lp =
 IO.puts(lp)
 # => Now is the time
 IO.puts(content)
+
---
+++ scopes.exs
@@ -0,0 +1,17 @@
+content = "Now is time"
+
+# _installassistant:*:25:25:Install Assistant:/var/empty:/usr/bin/false
+# _lp:*:26:26:Printing Services:/var/spool/cups:/usr/bin/false
+# _postfix:*:27:27:Postfix Mail Server:/var/spool/postfix:/usr/bin/false
+
+lp=
+    with {:ok, file} = File.open("example_password"),
+        content = IO.read(file, :all),
+        :ok = File.close(file),
+        [_, uid, gid] = Regex.run(~r/^_lp:.*?:(\d+):(\d+)/m, content)
+    do
+        "Group: #{gid} User: #{uid}"
+    end
+
+IO.puts lp
+IO.puts content
